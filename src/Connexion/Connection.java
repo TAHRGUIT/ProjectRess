e9e5c7050ce70fd618b01bf9631b298cfebea762
Y.T <Y.T@YasSin>
2016-02-15 01:30:17 +0000

    supprimit lol

---
+++ src/Connexion/Connection.java
@@ -0,0 +1,17 @@
+package Connexion;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Y.T
+ */
+public class Connection {
+    public java.sql.Connection con;
+    public Connection() throws SQLException, ClassNotFoundException{
+        Class.forName("com.mysql.jdbc.Driver");
+        con = (java.sql.Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ressources humaines","root","");
+
+    }
+}
